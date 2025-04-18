8c9ce13cb2a695a696c790c5a8d130489e13dcd5
LetaEmiru1 <106752902+LetaEmiru1@users.noreply.github.com>
2025-04-18 09:30:56 +0300

    Create 10-car.js
    #10 fixed

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+class Car {
+  constructor(_brand, _motor, _color){
+    this._brand = _brand;
+    this._motor = _motor;
+    this._color = _color;
+  }
+
+  cloneCar(){
+    return new this.constructor();
+  }
+}
